51077cb582c9a8293775b197b5f548388293481b
likoscp <Abdikhalikova_sabina@protonmail.com>
2024-11-17 21:41:24 +0500

    fix again

--- Pages/loadHeaderFooter.js
+++ Pages/loadHeaderFooter.js
@@ -8,12 +8,12 @@ function loadHTML(url, elementId) {
         .catch(error => console.error('Error loading HTML:', error));
 }
 
-loadHTML('./header.html', 'header');
+loadHTML('./Pages/header.html', 'header');
 
 
 document.addEventListener('DOMContentLoaded', function() {
 
-    loadHTML('./footer.html', 'footer').then(() => {
+    loadHTML('./Pages/footer.html', 'footer').then(() => {
 
         const modal = document.getElementById("contactModal");
         const contactLink = document.querySelector('a[href="#contactModalWindow"]');
