5ea4b2109cd45dca34246683167e02248572887e
likoscp <Abdikhalikova_sabina@protonmail.com>
2024-11-19 01:19:32 +0500

    fix sound

--- Js/play_sound.js
+++ Js/play_sound.js
@@ -10,51 +10,51 @@ const gacha = document.getElementById("gachaBtn");
 const adventure = document.getElementById("adventureBtn");
 
 mmorpg.addEventListener("click", () => {
-    let sound = new Audio("/sound/mmorpg.mp3");
+    let sound = new Audio("/dias-sabina/sound/mmorpg.mp3");
     sound.play();
 })
 
 mmo.addEventListener("click", () => {
-    let sound = new Audio("/sound/mmorpg.mp3");
+    let sound = new Audio("/dias-sabina/sound/mmorpg.mp3");
     sound.play();
 })
 
 apocalypse.addEventListener("click", () => {
-    let sound = new Audio("/sound/apocalypse.mp3");
+    let sound = new Audio("/dias-sabina/sound/apocalypse.mp3");
     sound.play();
 })
 
 story.addEventListener("click", () => {
-    let sound = new Audio("/sound/undertale.m4a");
+    let sound = new Audio("/dias-sabina/sound/undertale.m4a");
     sound.play();
 });
 
 pixel.addEventListener("click", () => {
-    let sound = new Audio("/sound/pixel.mp3")
+    let sound = new Audio("/dias-sabina/sound/pixel.mp3")
     sound.play();
 });
 
 shoouter.addEventListener("click", () => {
-    let sound = new Audio("/sound/shoouter.mp3")
+    let sound = new Audio("/dias-sabina/sound/shoouter.mp3")
     sound.play();
 });
 
 detective.addEventListener("click", () => {
-    let sound = new Audio("/sound/detective.mp3");
+    let sound = new Audio("/dias-sabina/sound/detective.mp3");
     sound.play();
 })
 
 fantasy.addEventListener("click", () => {
-    let sound = new Audio("/sound/fantasy.mp3");
+    let sound = new Audio("/dias-sabina/sound/fantasy.mp3");
     sound.play();
 })
 
 gacha.addEventListener("click", () => {
-    let sound = new Audio("/sound/fantasy.mp3");
+    let sound = new Audio("/dias-sabina/sound/fantasy.mp3");
     sound.play();
 })
 
 adventure.addEventListener("click", () => {
-    let sound = new Audio("/sound/adventure.mp3");
+    let sound = new Audio("/dias-sabina/sound/adventure.mp3");
     sound.play();
 })
