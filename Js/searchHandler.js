a2bd5cc87105b5445da0f80a683295bf05cf9b11
likoscp <Abdikhalikova_sabina@protonmail.com>
2024-11-19 07:40:26 +0500

    try

--- Js/searchHandler.js
+++
@@ -1,6 +0,0 @@
-document.getElementById('searchForm').addEventListener('submit', function(event) {
-    event.preventDefault(); 
-    var searchQuery = document.getElementById('searchQuery').value;
-    window.location.href = `/dias-sabina/docs/searchPage.html?search=${encodeURIComponent(searchQuery)}`;
-    
-  });
